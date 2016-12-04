be7764829a04c83c1d6a1e9bc0633a7b36aa1ce9
Homero Junior <homero_junior_oliveira@outlook.com>
2016-12-04 18:02:54 -0200

    Problema 1 resolvido

--- problems/problems.py
+++ problems/problems.py
@@ -1,3 +1,9 @@
+# 1)Multiples of 3 and 5
+def problem1():
+    total = sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
+    print(total)
+
+
 # 6) Sum square difference
 def problem6():
     numbers = list(range(1, 101))
@@ -6,4 +12,4 @@ def problem6():
 
 
 if __name__ == '__main__':
-    problem6()
+    problem1()
